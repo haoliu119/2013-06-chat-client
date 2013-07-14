c6b64794152cd6658d62a4caad66dbdddf724ba0
Hao Liu <haoliu119@gmail.com>
2013-07-14 13:14:48 -0700

    Refactored: Index, CSS, Setup.js to be pretty. Added forms for submission

--- js/setup.js
+++ js/setup.js
@@ -1,56 +1,61 @@
-var friendlist = {};
+(function(){
+var friendList = {};
 var chatRooms = {
   currentChatroom: 'messages'
 };
 
-var serverURL = 'http://10.0.1.19:8080/classes/';
+var serverURL = 'http://127.0.0.1:8080/classes/';
 
 if(!/(&|\?)username=/.test(window.location.search)){
   var newSearch = window.location.search;
   if(newSearch !== '' & newSearch !== '?'){
     newSearch += '&';
   }
-  newSearch += 'username=' + (prompt('What is your name?') || 'anonymous');
+  var username = prompt('your username? (letters and numbers only): ');
+  while (!username || !/^[a-zA-Z0-9]+$/.test(username)){
+    var username = prompt('Invalid characters.\nyour username? (letters and numbers only): ');
+  }
+  newSearch += 'username=' + username;
   window.location.search = newSearch;
 }
-
+var user = window.location.search.slice(10);
+$('#userName').text(user);
 // Don't worry about this code, it will ensure that your ajax calls are allowed by the browser
 // $.ajaxPrefilter(function(settings, _, jqXHR) {
 //   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
 //   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 // });
-
+/**
+  *
+  */
 function print(data){
   data = JSON.parse(data);
-  $('.chats').text('');
-  _.each(data['results'], function(val){
-    var $text = val['text'] ? $("<span class='chatMessage'></span>").text(val['text'].slice(0,200)) : "";
-    if (friendlist[val['username']] && $text) {$text.addClass('bold');}
+  $('#chatList').text('');
+  _.each(data, function(val){
     var $userLink = $("<a href='#' class='userLink'></a>").text(val['username']);
-    var $timeStamp = $("<span class='timeStamp'></span>").text(val['createdAt']);
-    var newMessage = $('<li></li>').append($userLink).append($text).append($timeStamp);
-    $('.chats').append(newMessage);
+    var $timeStamp = $("<span class='timeStamp'></span><br>").text(prettyTimeStamp(val['createdAt']));
+    var $text = val['text'] ? $("<span class='chatMessage'></span>").text(val['text'].slice(0,200)) : "";
+    if (friendList[val['username']] && $text) {
+      $text.addClass('bold');
+    }
+    var newMessage = $('<li></li>').append($userLink).append($timeStamp).append($text);
+    $('#chatList').append(newMessage);
   });
 }
-function randomColor(){
-  return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+
+function prettyTimeStamp(timeStamp){
+  var now = new Date(),
+  dif = Math.floor((now-timeStamp)/1000); // dif in seconds
+
+  if (dif < 2){
+    return dif + " second ago";
+  }else if (dif < 60){
+    return dif + " seconds ago"
+  }else{
+    return moment(timeStamp).fromNow(); // function defined in MIT's mement.min.js
+  }
 }
-// setInterval(function () {
-//     $('body').css({'background': randomColor()});
-//     $('.friendlist').css({'background': randomColor(), 'color':randomColor()});
-//     $('.chatrooms').css({'background': randomColor(), 'color': randomColor()});
-//     $('.chats').css({'background': randomColor(), 'color': randomColor()});
-//     $('#main').css({'background': randomColor(), 'color': randomColor()});
-//     $('.userLink').css({'background': randomColor(), 'color': randomColor()});
-//     $('.bold').css({'background': randomColor(), 'color': randomColor()});
-//     $('.friendlistbox').css({'background': randomColor(), 'color': randomColor()});
-//     $('#input').css({'background': randomColor(), 'color': randomColor()});
-//     $('#submit').css({'background': randomColor(), 'color': randomColor()});
-//     $('#refresh').css({'background': randomColor(), 'color': randomColor()});
-//     $('.asidearea').css({'background': randomColor(), 'color': randomColor()});
-//     $('h1').css({'background': randomColor(), 'color': randomColor()});
-//     $('h2').css({'background': randomColor(), 'color': randomColor()});
-// }, 1000);
+
 var timeID;
 var $get = function(){
   var roomName = chatRooms.currentChatroom, url = serverURL + roomName;
@@ -58,8 +63,8 @@ var $get = function(){
     // contentType: 'application/json',
     type: 'GET',
     data: {
-      // 'order':'-createdAt',
-      // 'limit': '30'
+    // 'order':'-createdAt',
+    // 'limit': '30'
     },
     success: print,
     error: function(data) {
@@ -68,62 +73,80 @@ var $get = function(){
   });
   timeID = setTimeout($get, 2000);
 };
-clearTimeout(timeID);
-$get();
+
 
 var $post = function(message){
-  var user = window.location.search.slice(10), roomName = chatRooms.currentChatroom, url = serverURL + roomName;
-  message = message || "empty message";
-  //user = "a_s_d_f" || "<script type='text/javascript'>window.close()</script>";
-  $.ajax(url, {
-    // contentType: 'application/json',
-    type: 'POST',
-    data: JSON.stringify({
-/*jshint multistr: true */
-      'text' : message,
-      'username': user
-    }),
-    success: function(data){
-      console.log('POSTED');
-    },
-    error: function(data) {
-      console.log('Ajax POST failed');
-    }
-  });
-  clearTimeout(timeID);
-  $get(roomName);
+  var roomName = chatRooms.currentChatroom, url = serverURL + roomName;
+// message = message || "empty message";
+$.ajax(url, {
+// contentType: 'application/json',
+type: 'POST',
+data: JSON.stringify({
+  /*jshint multistr: true */
+  'text' : message,
+  'username': user
+}),
+success: function(data){
+  console.log('POSTED');
+},
+error: function(data) {
+  console.log('Ajax POST failed');
+}
+});
+clearTimeout(timeID);
+$get(roomName);
 };
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * Event Listeners
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 
+/** Click Username:
+ ** Add User to Fiend List,
+ ** Hightight Friend's Messages **/
 $('body').on('click','.userLink',function(event){
   event.preventDefault();
-  friendlist[$(this).text()] = friendlist[$(this).text()] ? false : true;
-  if (friendlist[$(this).text()]) { $('.friendlist').append('<li>'+$(this).text()+'</li>'); }
-  else { $('.friendlist').find($('li:contains('+$(this).text()+')')).remove(); }
-  clearTimeout(timeID);
-  $get();
-});
-
-$('.changeRoom').on('click', function(){
-  var roomName = $('.chatroomField').val().split(" ").join('_');
-  if (chatRooms.currentChatroom !== roomName && !chatRooms[roomName]){
-    chatRooms.currentChatroom = roomName;
-    chatRooms[roomName] = true;
-    $('.chatrooms').append('<li><a href="#">' + roomName + '</a></li>');
+  if($(this).text() !== user){
+    friendList[$(this).text()] = friendList[$(this).text()] ? false : true;
+    if (friendList[$(this).text()]) { $('#friendList').append('<li>'+$(this).text()+'</li>'); }
+    else { $('#friendList').find($('li:contains('+$(this).text()+')')).remove(); }
     clearTimeout(timeID);
-    $get(roomName);
-    $('#chatroomName').text(roomName);
-    $('.chatroomField').val("");
+    $get();
   }
 });
 
-$('.chatrooms').on('click','li', function(){
+$('#createChatRoomForm').submit(function(event){
+  event.preventDefault();
+  var roomName = $('#createChatRoom').val().split(" ").join('_'); // TODO: need regex checker function
+  if (chatRooms.currentChatroom !== roomName && !chatRooms[roomName]){
+    chatRooms.currentChatroom = roomName;
+    chatRooms[roomName] = true;
+    $('#chatRooms').append('<li><a href="#">' + roomName + '</a></li>');
+    clearTimeout(timeID);
+    $get(roomName);
+    $('#chatRoomName').text(roomName);
+    $('#createChatRoom').val("");
+  }
+});
+
+/** Click Chatroom Name to Switch Chatroom **/
+$('#chatRooms').on('click','li', function(event){
+  event.preventDefault();
   chatRooms.currentChatroom = $(this).text();
-  $('#chatroomName').text($(this).text());
+  $('#chatRoomName').text($(this).text());
   clearTimeout(timeID);
   $get();
 });
 
-$('#submit').click(function(){
-  $post($('#input').val());
-  $('#input').val('');
+/** Get User Chat Input, POST to Server **/
+$('#userChatForm').submit(function(event){
+  event.preventDefault();
+  $post($('#userChatMessage').val());
+  $('#userChatMessage').val('');
 });
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * Initial Get + Print Funciton Call
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+clearTimeout(timeID);
+$get();
+})();
